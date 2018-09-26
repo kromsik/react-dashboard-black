2f1df76678906fb614b291d0b488f80d0997c6d2
kromsik <kromsik@pm.me>
2018-09-26 11:25:03 +0300

    init and make it work

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+import styled from 'styled-components';
+
+const Logo = styled.div`
+  font-size: 1.5em;
+`
+
+const ControlButton = styled.div`
+`
+
+const AppLayout = styled.div`
+  padding: 40px;
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+`
+
+class App extends Component {
+  render() {
+    return (
+      <AppLayout>
+        <Logo>
+          CryptoDash
+        </Logo>
+        <div>
+        </div>
+        <ControlButton>
+          Dashboard
+        </ControlButton>
+        <ControlButton>
+          Settings
+        </ControlButton>
+      </AppLayout>
+    );
+  }
+}
+
+export default App;
