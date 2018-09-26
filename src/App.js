02119f8c70c785c1187451ececa7b735179b6531
kromsik <kromsik@pm.me>
2018-09-26 11:46:35 +0300

    add control buttons

--- src/App.js
+++ src/App.js
@@ -1,38 +1,63 @@
-import React, { Component } from 'react';
-import './App.css';
-import styled from 'styled-components';
+import React, { Component } from 'react'
+import './App.css'
+import styled, { css } from 'styled-components'
 
 const Logo = styled.div`
-  font-size: 1.5em;
+  font-size: 1.6rem;
 `
 
 const ControlButton = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 1px 1px 3rem #03ff03;
+      font-size: 1.6rem;
+    `};
 `
 
 const AppLayout = styled.div`
-  padding: 40px;
-  display: grid;
-  grid-template-columns: 180px auto 100px 100px;
+  padding: 4rem;
+  font-size: 1.5rem;
 `
 
+const AppBar = styled.div`
+  display: grid;
+  grid-template-columns: 180px auto 100px 100px;
+  margin-bottom: 4rem;
+`
+
+const Content = styled.div``
+
 class App extends Component {
+  state = {
+    page: `dashboard`
+  }
+  displayingDashboard = () => this.state.page === `dashboard`
+  displayingSettings = () => this.state.page === `settings`
   render() {
     return (
       <AppLayout>
-        <Logo>
-          CryptoDash
-        </Logo>
-        <div>
-        </div>
-        <ControlButton>
-          Dashboard
-        </ControlButton>
-        <ControlButton>
-          Settings
-        </ControlButton>
+        <AppBar>
+          <Logo>CryptoDash</Logo>
+          <div />
+          <ControlButton
+            onClick={() => this.setState({ page: `dashboard` })}
+            active={this.displayingDashboard()}
+          >
+            Dashboard
+          </ControlButton>
+          <ControlButton
+            onClick={() => this.setState({ page: `settings` })}
+            active={this.displayingSettings()}
+          >
+            Settings
+          </ControlButton>
+        </AppBar>
+        <Content>Hello, I'm {this.state.page}</Content>
       </AppLayout>
-    );
+    )
   }
 }
 
-export default App;
+export default App
